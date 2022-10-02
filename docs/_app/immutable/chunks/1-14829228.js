23e43b307ce244bd84b2d64d682492a570af0710
Scott Stephens <scott@nuruq.com>
2022-10-02 15:06:18 -0700

    add some words about Brunei

--- docs/_app/immutable/chunks/1-14829228.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/error.svelte-0ad0fea6.js";export{t as component};
---
+++ docs/_app/immutable/chunks/1-a168aa0a.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-af45c097.js";export{t as component};
