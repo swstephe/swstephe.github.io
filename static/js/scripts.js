357aa888c1d3262ac5a87e693aba790e8b4fea3b
Scott Stephens <scott@nuruq.com>
2022-10-02 13:35:46 -0700

    removed old vanilla javascript

--- static/js/scripts.js
+++
@@ -1,9 +0,0 @@
-const navbar = document.getElementsByClassName('navbar')[0];
-const hasOpen = new RegExp('\\bopen\\b');
-
-function toggleNavMenu() {
-  if (navbar.className.match(hasOpen))
-    navbar.className = navbar.className.replace(hasOpen, '');
-  else
-    navbar.className = navbar.className + " open";
-}
